63ab2a50d89f3deb3466b3381972e6b4374d2757
Karluwa <kkarlina@mail.ru>
2023-04-29 10:49:59 +0300

    Task1

---
+++ task1/Task3.java
@@ -0,0 +1,7 @@
+public class Task3 {
+    public static void main(String[] args) {
+        int time = 13;
+        boolean day = time >= 6 || time <= 22;
+        boolean isGoodwether = true;
+    }
+}
